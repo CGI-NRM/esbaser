ba512e6ed657d951ed82015d01048fabd830e3ab
Elias Lundell <elilun03@gmail.com>
2023-06-07 16:16:33 +0200

    Init setup of empty files

---
+++ R/utils.R
@@ -0,0 +1,13 @@
+#' Load All the Data
+#'
+#' Load all the data and do some magic with it.
+#'
+#' @param path The path to the csv file
+#'
+#' @return Parsed data
+#' @export
+#' @importFrom shiny NS
+#' @examples
+#' data <- load_data("~/data.csv")
+load_data <- function(path) {
+}
