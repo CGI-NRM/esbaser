4ef89408d03e8631940ce714351cbd40f2bd9303
Elias Lundell <elilun03@gmail.com>
2023-06-12 22:25:36 +0200

    Handle empty accnr

--- R/db.R
+++ R/db.R
@@ -8,8 +8,6 @@
 #' @importFrom stats runif
 #' @export
 get_accnr_biologdata <- function(accnr_str) {
-  accnr_list <- accnr_parse(accnr_str)
-  set.seed(accnr_list$value)
   colnames <- c("Annat NRMnr.", "Acc.nr.", "Ålder (år)",
                 "Kroppsvikt (g)", "Totallängd (cm)",
                 "Kroppslängd (cm)", "Kön", "Gonadvikt (g)",
@@ -17,23 +15,30 @@ get_accnr_biologdata <- function(accnr_str) {
                 "Parasit (g)", "Skrottvikt (g)", "Mage sparad J/N",
                 "Notering/Avvikelse")
 
-  df <- data.frame(
-    list("",
-         accnr_str,
-         runif(1, 0, 10),
-         runif(1, 120, 250),
-         runif(1, 8, 30),
-         runif(1, 9, 24),
-         sample(c("M", "F"), 1),
-         runif(1, 2, 10),
-         sample(c("J", "N"), 1),
-         runif(1, 4, 12),
-         runif(1, 2, 10),
-         runif(1, 5, 100),
-         runif(1, 4, 10),
-         sample(c("J", "N"), 1),
-         ""
-    ))
+  if (accnr_str == "") {
+    df <- data.frame(list(rep("", length(colnames))))
+  } else {
+    accnr_list <- accnr_parse(accnr_str)
+    set.seed(accnr_list$value)
+    df <- data.frame(
+      list("",
+           accnr_str,
+           runif(1, 0, 10),
+           runif(1, 120, 250),
+           runif(1, 8, 30),
+           runif(1, 9, 24),
+           sample(c("M", "F"), 1),
+           runif(1, 2, 10),
+           sample(c("J", "N"), 1),
+           runif(1, 4, 12),
+           runif(1, 2, 10),
+           runif(1, 5, 100),
+           runif(1, 4, 10),
+           sample(c("J", "N"), 1),
+           ""
+      ))
+  }
+
   colnames(df) <- colnames
   df
 }
