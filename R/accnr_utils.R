884162980fcb34aab510196f541f83b55975d045
Elias Lundell <elilun03@gmail.com>
2023-06-15 14:00:55 +0200

    Use list of allowed chars to allow to test for provid

--- R/accnr_utils.R
+++ R/accnr_utils.R
@@ -5,11 +5,14 @@
 #' @return Logical value wether the accnr_str is a valid accnr that can be parsed or not.
 #'
 #' @param accnr_str The AccNR to be validated
+#' @param valid_letters Which letters to accept as the first.
+#' Default is ABCDGHLXP, which is the requirement for later conversion to database format.
+#' But could also be Q for ProvIDs.
 #'
 #' @importFrom stringr str_detect
 #' @export
-accnr_validate <- function(accnr_str) {
-  str_detect(accnr_str, "^[ABCDGHLXP][\\d]{4}/?[\\d]{5}$")
+accnr_validate <- function(accnr_str, valid_letters = "ABCDGHLXP") {
+  str_detect(accnr_str, paste0("^[", paste0(valid_letters, collapse = ""), "][\\d]{4}/?[\\d]{5}$"))
 }
 
 
@@ -31,10 +34,13 @@ accnr_add <- function(accnr_list, amount) {
 #' @return A list containing the 'letter', 'year' and numeric 'value' of the accnr
 #'
 #' @param accnr_str The AccNR to be parsed, either with or without an '/'
+#' @param valid_letters Which letters to accept as the first.
+#' Default is ABCDGHLXP, which is the requirement for later conversion to database format.
+#' But could also be Q for ProvIDs.
 #' @export
-accnr_parse <- function(accnr_str) {
-  if (!accnr_validate(accnr_str)) {
-    return(list(letter = "A", year = 0, value = 0))
+accnr_parse <- function(accnr_str, valid_letters = "ABCDGHLXP") {
+  if (!accnr_validate(accnr_str, valid_letters)) {
+    return(NULL)
   }
 
   letter <- substring(accnr_str, 1, 1)
@@ -71,6 +77,10 @@ accnr_sprint <- function(accnr_list) {
 #' @return A string of the accnr formated for the db
 #' @export
 accnr_to_database_format <-  function(accnr_list) {
+  if (!(accnr_list$letter %in% c("A", "B", "C", "D", "G", "H", "L", "X", "P"))) {
+    warning("AccNR's letter was not one of ABCDGHLXP and cannot be converted to database format.")
+    return(NULL)
+  }
   year_str <- sprintf("%04d", accnr_list$year)
   first_num <- list("A" = "1", "B" = "2", "C" = "3", "D" = "4", "G" = "7", "H" = "8", "L" = "5", "P" = "9", "X" = "6")[[accnr_list$letter]]
   sprintf("%s%s%05d", first_num, substring(year_str, 2, 4), accnr_list$value)
@@ -82,6 +92,10 @@ accnr_to_database_format <-  function(accnr_list) {
 #' @return A parsed accnr_list
 #' @export
 accdb_parse_to_accnr <- function(accdb_str) {
+  if (!str_detect(accdb_str, "[1-9][0-9]{8}")) {
+    warning("AccDB str is not correctly formatted and cannot be converted to AccNR.")
+    return(NULL)
+  }
   letter <- list(
     "1" = "A", "2" = "B", "3" = "C", "4" = "D", "7" = "G",
     "8" = "H", "5" = "L", "9" = "P", "6" = "X"
