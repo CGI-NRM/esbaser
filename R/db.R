58e1bcc7306a20b51fdf3ab487c5f980e6780f70
Elias Lundell <elilun03@gmail.com>
2023-07-05 11:34:53 +0200

    Allow passing other ip, username and password

--- R/db.R
+++ R/db.R
@@ -6,15 +6,23 @@ NULL
 #' Initializes connection to database.
 #'
 #' @param dbname The name of the database, is 'test' on my local computer, 'mgg2' on the production machine
+#' @param host The ip of the mariadb database to connect to
+#' @param username The username when connecting to database
+#' @param password The password when connecting to database
 #' @return conn The database connection
 #' @importFrom DBI dbConnect
 #' @export
-connect_to_database <- function(dbname = "test") {
+connect_to_database <- function(
+  host = "172.17.0.1",
+  username = "docker",
+  password = "dockerpass",
+  dbname = "test"
+) {
   conn <- DBI::dbConnect(
     RMariaDB::MariaDB(),
-    username = "docker",
-    password = "dockerpass",
-    host = "172.17.0.1",
+    username = username,
+    password = password,
+    host = host,
     dbname = dbname
   )
 
